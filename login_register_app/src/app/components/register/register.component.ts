01297f30df056cdb5e9db4ae0d917179172bfecc
Subrahmanyam956 <subrahmanyam956@gmail.com>
2024-03-10 21:25:53 +0530

    adding a dummy backend server, login and register components and routing to home for a valid users

--- login_register_app/src/app/components/register/register.component.ts
+++ login_register_app/src/app/components/register/register.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { passwordMatchValidator } from '../../shared/password-match.directive';
+import { AuthService } from '../../services/auth.service';
+import { User } from '../../interfaces/auth';
+import { MessageService } from 'primeng/api';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-register',
@@ -18,7 +23,12 @@ export class RegisterComponent {
     validators: passwordMatchValidator
   });
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private messageService: MessageService,
+    private router: Router
+    ) {}
 
   get fullName() {
     return this.registerForm.controls['fullName'];
@@ -34,5 +44,20 @@ export class RegisterComponent {
 
   get confirmPassword() {
     return this.registerForm.controls['confirmPassword'];
+  }
+
+
+  submitDetails() {
+    const postData = { ... this.registerForm.value };
+    delete postData.confirmPassword;
+    this.authService.registerUser(postData as User).subscribe(
+      response => {
+          this.messageService.add({ severity: 'success', summary: 'success', detail: 'Register Successfull'});
+          this.router.navigate(['login'])
+      },
+      error => {
+        this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Something went wrong'});
+      }
+    )
   }
 }
